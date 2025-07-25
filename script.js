03d9c2d9acaea4e2816f3dc3ac8430dea4469d11
PEC75012 <pecharon@gmail.com>
2025-07-25 11:13:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -94,6 +94,9 @@ document.addEventListener("DOMContentLoaded", function () {
     data["La transformation écologique pour une ville vivable et désirable"] = interets.includes("La transformation écologique pour une ville vivable et désirable") ? "Oui" : "";
     data["Une ville apaisée pour une meilleure qualité de vie au quotidien"] = interets.includes("Une ville apaisée pour une meilleure qualité de vie au quotidien") ? "Oui" : "";
     data["La culture, levier d’émancipation et de partage"] = interets.includes("La culture, levier d’émancipation et de partage") ? "Oui" : "";
+    data["Une capitale ouverte sur sa métropole, motrice de l’intérêt général"] =
+  interets.includes("Une capitale ouverte sur sa métropole, motrice de l’intérêt général") ? "Oui" : "";
+
     data["Une ville du soin et de la solidarité"] = interets.includes("Une ville du soin et de la solidarité") ? "Oui" : "";
     data["Redonner du souffle au débat démocratique"] = interets.includes("Redonner du souffle au débat démocratique") ? "Oui" : "";
     data["Paris, capitale de l’égalité et de la lutte contre toutes les discriminations"] = interets.includes("Paris, capitale de l’égalité et de la lutte contre toutes les discriminations") ? "Oui" : "";
